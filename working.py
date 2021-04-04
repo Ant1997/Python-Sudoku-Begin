8da181894364dbe18dc3af6ed65a77f073179548
Ant1997 <anthonyhuynh997@gmail.com>
2021-04-03 20:16:31 -0500

    Find Empty Position, Check Validity in Row

--- working.py
+++ working.py
@@ -11,3 +11,35 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+def valid(boardIterate, number, position):
+    # Check row
+    for i in range(len(boardIterate[0])):
+        if boardIterate[pos[0]][i] == number and position[1] != i:
+            return False
+
+def print_board(boardIterate):
+    #print(len(boardIterate))
+    for i in range(len(boardIterate)): # for loop; going from 0 to total length of 'board', which is 9. Stops at 9. 
+        #0, 1, 2,... 8 (essentially for (int i = 0; i < 9; i++))
+        if i % 3 == 0 and i != 0: #if i is the third row, then print a line. Ignore the first line.
+            print("-----------------------")
+        
+        for j in range(len(boardIterate[0])): # columns. Looking at total values/columns for the first row.
+            if j % 3 == 0 and j != 0: # if column is the third but not the first
+                print (" | ", end="") # print's end parameter disable newline
+
+            if j == 8: # if last column then 
+                print(boardIterate[i][j]) # By default Python print ends with newline.
+            else:
+                print(str(boardIterate[i][j]) + " ", end="") # Not the last column so it continue to print the value
+
+
+def find_empty(boardIterate):
+    for i in range(len(boardIterate)):
+        for j in range(len(boardIterate[0])):
+            if bo[i][j] == 0:
+                return (i,j)
+                
+
+print_board(board)
